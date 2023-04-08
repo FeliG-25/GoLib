51e17de2abf16b485fd6b12b5039066621b9b2be
Maycelline S <maycellineselvyanti@gmail.com>
2023-04-08 12:23:23 +0700

    login and authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,31 @@
-const user = require('./../models/userModel');
-const member = require('./../models/memberModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+
+const User = require('./../models/userModel');
+const Member = require('./../models/memberModel');
+
+dotenv.config({path: './config.env'});
 
 
 exports.createUser = async (req, res) => {
+    const { user_name, password, email, user_type} = req.body;
     try {
-        const newMember = await member.create(req.body);
-        const newUser = await user.create(req.body);
+
+        // Check if username is already taken
+        const existingUser = await User.findOne({ user_name });
+        if (existingUser){
+            return res.status(400).json({
+                status: 'fail',
+                message: 'Username is already taken'
+            });
+        }
+
+        const newMember = await Member.create(req.body);
+        const newUser = new User({user_name, password, email, user_type})
+        await newUser.save();
+        
+        // const newUser = await User.create(req.body);
         
         res.status(201).json({
             status: 201,
@@ -15,6 +35,48 @@ exports.createUser = async (req, res) => {
         res.status(400).json({
             status:'Register Failed!',
             message: err
+        });
+    }
+}
+
+
+exports.login = async (req, res) => {
+    const { user_name, password } = req.body;
+
+    try {
+        const user = await User.findOne({user_name});
+        const member = await Member.findOne({user_name});
+
+        //If there is no user
+        if (!user && !member){
+            return res.status(401).json({
+                status: '401',
+                message: 'Invalid credential'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({
+                status: 400,
+                message: 'Invalid Password'
+            });
+        }
+
+        const payload = {user: {id: user.id}};
+        const token = jwt.sign(payload, process.env.SECRET, {expiresIn: 3000000});
+
+        res.status(300).json({
+            status:200,
+            user: user,
+            member: member,
+            token: token
+        })
+
+    } catch (err){
+        console.error(err);
+        res.status(500).json({
+            message: 'Internal server error'
         });
     }
 }
