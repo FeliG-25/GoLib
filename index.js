ea41749b74df04a50094f7c85534abae52ba62fb
brianChristoph <bbchristopher19@gmail.com>
2023-04-11 21:48:31 +0700

    Book Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routes/userRoutes');
 const adminRouter = require('./routes/adminRoutes');
+const bookRouter = require('./routes/bookRoutes');
 
 const app = express()
 
@@ -18,5 +19,8 @@ app.use('/api/v1', userRouter)
 
 // routes for admin
 app.use('/api/v1/admin', adminRouter)
+
+// routes for books (users & members can view)
+app.use('/api/v1/book', bookRouter)
 
 module.exports = app
