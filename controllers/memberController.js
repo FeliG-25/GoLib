ae44bc7a6c1cf1749aa351cb0506ed8a8352b91a
brianChristoph <bbchristopher19@gmail.com>
2023-04-14 22:37:34 +0700

    Correction Get Cart

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const dotenv = require('dotenv')
 
+const User = require('./../models/userModel')
 const Member = require('./../models/memberModel')
+const Cart = require('./../models/cartModel')
 
 const mongoose = require('mongoose')
 
@@ -10,7 +12,9 @@ dotenv.config({path: './config.env'});
 
 exports.getUserCart = async (req, res) => {
     try {
-        const cart = await Member.findById(req.params.id).select("cart")
+        const user = await User.findById(req.params.id);
+        const member = await Member.findOne({'member_id':user._id})
+        const cart = await Cart.findOne({'_id':member.cart});
         
         res.status(200).json({
             status: '200',
