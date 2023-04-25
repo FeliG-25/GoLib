00d7e999751610e3542b0984344253cf8520b01c
Maycelline S <maycellineselvyanti@gmail.com>
2023-04-25 15:58:25 +0700

    auth member & admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,22 +8,30 @@ const cookieParser = require('cookie-parser');
 const app = express()
 app.use(cookieParser())
 
-function auth (req, res, next) {
-    const token = req.cookies.token;
-    if(token) {
-        req.headers['Authorization'] = `Bearer ${token}`;
-    } else if(!token) {
-        return res.status(401).json({msg: 'Please login first'})
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.SECRET)
-        req = decoded;
-        next()
-    } catch (err) {
-        console.log(err)
-        res.status(401).json({msg: "Token is not valid"})
-    }
+function auth(role) {
+    return function(req, res, next) {
+        const token = req.cookies.token;
+        const user = JSON.parse(req.cookies.user);
+        if(token) {
+          req.headers['Authorization'] = `Bearer ${token}`;
+          console.log(user)
+          console.log(user.user_type)
+        } else if(!token) {
+          return res.status(401).json({msg: 'Please login first'})
+        }
+        
+        try {
+          const decoded = jwt.verify(token, process.env.SECRET)
+          req = decoded;
+          if (role !== user.user_type) {
+            return res.status(403).json({msg: 'Access denied'});
+          }
+          next()
+        } catch (err) {
+          console.log(err)
+          res.status(401).json({msg: "Token is not valid"})
+        }
+      }
 }
 
 module.exports = auth;
